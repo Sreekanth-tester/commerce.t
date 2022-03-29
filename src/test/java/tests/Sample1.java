e3cb72b2eddc6857ba9b42c400523d2d1fe3c8c4
Sai Sreekanth Reddy <Sai Sreekanth Reddy@LAPTOP-NUSCDGP7>
2022-03-29 11:07:53 +0530

    swapping of two interger values were done

---
+++ src/test/java/tests/Sample1.java
@@ -0,0 +1,21 @@
+package tests;
+
+public class Sample1 
+{
+
+	public static void main(String[] args) 
+	{
+		int x=17;
+		int y=7;
+		//swap two numbers
+		
+		x=x+y;          // x=24
+		y=x-y ;          // y=17;   24-7
+		x=x-y;           //x= 7     24-17
+	 System.out.printf("values are %d and  %d",x,y);
+		
+		
+
+	}
+
+}
